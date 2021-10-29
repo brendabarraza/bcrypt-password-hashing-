59060b72d06a05552a0dc9ca966383b5b901a64e
Brenda Barraza <74740129+brendabarraza@users.noreply.github.com>
2021-10-29 02:11:07 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+
+	password := ""
+  
+	fmt.Print("new password:")
+	fmt.Scan(&password)
+	/*fmt.Printf("your password is: %v", password)*/
+
+	encrypted, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	
+  if err != nil {
+		fmt.Println(err)
+	}
+	
+  fmt.Println(password)
+	fmt.Println(encrypted)
+
+	/*password hash, to save in BD*/
+	fmt.Println(string(encrypted))
+
+	loginPass := ""
+	 
+  fmt.Print("login with your password:")
+	fmt.Scan(&loginPass)
+	/*fmt.Printf("your password is: %v", loginPass)*/
+
+	err = bcrypt.CompareHashAndPassword(encrypted, []byte(loginPass))
+	
+  if err != nil {
+		fmt.Println("LOGIN ERROR!!!!!!!!!!!")
+		return
+	}
+  fmt.Println("YOU ARE SUCCESSFULLY LOG :)")
+
+}
